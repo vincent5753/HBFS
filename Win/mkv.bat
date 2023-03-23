edbdb74300f176e96eb220747ab779ba7931fff5
VP <51seer.vincent@gmail.com>
2023-03-24 02:22:53 +0800

    Added nvidia encode for mkv compress batch.
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- Win/mkv.bat
+++ Win/mkv.bat
@@ -1,3 +1,5 @@
 @echo off
 @title Converting avi to mkv...
 FOR %%f IN (*.avi) DO ffmpeg -i "%%f" -c:v libx265 -preset slow -c:a flac -y "%%~nf.mkv"
+rem If you have a nvidia card, use the following code instead.
+rem FOR %%f IN (*.avi) DO ffmpeg -i "%%f" -c:v hevc_nvenc -preset slow -c:a flac -y "%%~nf.mkv"
