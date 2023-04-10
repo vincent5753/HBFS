a43f0a38165fa93da80817c7d3fa761aeeea83f8
VP <51seer.vincent@gmail.com>
2023-04-11 00:34:06 +0800

    Changed encoding for nirvana.bat
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- Win/nirvana.bat
+++ Win/nirvana.bat
@@ -1,4 +1,4 @@
-rem �·t�Ҧ� / Dark Mode
+rem 黑暗模式 / Dark Mode
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t reg_DWORD /d "0" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t reg_DWORD /d "0" /f
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUseLightTheme" /t reg_DWORD /d "0" /f
