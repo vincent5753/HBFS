411f254ef1fc57c56c2a9a752d7f9e5b6bfb72db
VP <51seer.vincent@gmail.com>
2023-04-15 23:23:22 +0800

    Added registry tweak ColorPrevalence.
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- Win/nirvana.bat
+++ Win/nirvana.bat
@@ -7,3 +7,5 @@ rem 讚讚ㄉ透明 / Transparency Effects
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableTransparency" /t reg_DWORD /d "1" /f
 rem 不要模糊效果 / No Blur Effects
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableBlurBehind" /t reg_DWORD /d "0" /f
+rem 顏色跟著變 / 
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "ColorPrevalence" /t reg_DWORD /d "1" /f
