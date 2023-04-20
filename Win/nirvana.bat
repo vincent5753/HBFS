ebc952215db99bf6686ebb51227dbcd904cddbac
VP <51seer.vincent@gmail.com>
2023-04-20 15:32:55 +0800

    Added registry tweak Show Seconds In Taskbar SystemClock.
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- Win/nirvana.bat
+++ Win/nirvana.bat
@@ -9,3 +9,5 @@ rem 不要模糊效果 / No Blur Effects
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableBlurBehind" /t reg_DWORD /d "0" /f
 rem 工作列顏色跟著變 / Colorized Taskbar without colorized Start Menu and Action Center
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "ColorPrevalence" /t reg_DWORD /d "1" /f
+rem 工作列時鐘顯示秒數 / Show Seconds In Taskbar SystemClock
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowSecondsInSystemClock" /t reg_DWORD /d "1" /f
