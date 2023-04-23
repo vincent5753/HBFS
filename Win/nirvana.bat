2fc783bbe1e400fc36b6502eccf8d69f888bf16a
VP <51seer.vincent@gmail.com>
2023-04-24 00:08:37 +0800

    Added Tweak Section.
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- Win/nirvana.bat
+++ Win/nirvana.bat
@@ -1,3 +1,4 @@
+:: Explorer Tweaks
 rem 黑暗模式 / Dark Mode
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t reg_DWORD /d "0" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t reg_DWORD /d "0" /f
