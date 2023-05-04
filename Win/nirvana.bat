675b50ea98d87d53311e0f053bb8b37bfeda6610
VP <51seer.vincent@gmail.com>
2023-05-05 03:24:38 +0800

    Added Debloat Section.
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- Win/nirvana.bat
+++ Win/nirvana.bat
@@ -30,3 +30,5 @@ rem 關掉時間軸 / Disable Timeline
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableActivityFeed" /t REG_DWORD /d "0" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "PublishUserActivities" /t REG_DWORD /d "0" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "UploadUserActivities" /t REG_DWORD /d "0" /f
+
+:: Debloat
