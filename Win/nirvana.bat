a2cd3fd3b93af7035a65efd9b78b5b60b4150dbd
VP <51seer.vincent@gmail.com>
2023-07-15 15:33:05 +0800

    Got updated, just lookup commit yourself.
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- Win/nirvana.bat
+++ Win/nirvana.bat
@@ -31,4 +31,4 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableActivityFeed
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "PublishUserActivities" /t REG_DWORD /d "0" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "UploadUserActivities" /t REG_DWORD /d "0" /f
 
-:: Debloat
+:: Debloat 
