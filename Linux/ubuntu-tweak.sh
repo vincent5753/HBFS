88c3f150a3240f1da6d5a3d81d0f662a1fb4f46e
VP <51seer.vincent@gmail.com>
2023-07-23 01:45:30 +0800

    Updated
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- Linux/ubuntu-tweak.sh
+++ Linux/ubuntu-tweak.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# disable unattended upgrades
+sudo systemctl stop unattended-upgrades
+sudo systemctl disable unattended-upgrades
+# sudo apt-get purge unattended-upgrades
+
 # install packages
-sudo apt update
-sudo apt install -y openssh-server htop tree jq
+sudo apt-get update
+sudo apt-get install -y openssh-server htop tree jq
