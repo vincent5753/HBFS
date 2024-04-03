368d0de2779fabac9c77640865cb7fffe6957f15
VP <51seer.vincent@gmail.com>
2024-04-03 23:55:52 +0800

    Update ubuntu-tweak.sh

--- Linux/ubuntu-tweak.sh
+++ Linux/ubuntu-tweak.sh
@@ -4,6 +4,8 @@
 sudo systemctl stop unattended-upgrades
 sudo systemctl disable unattended-upgrades
 # sudo apt-get purge unattended-upgrades
+# I am lazy typing passwds
+sudo sed -i 's/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
 
 # Install packages
 sudo apt-get update
