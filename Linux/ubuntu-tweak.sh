26a4269b14f1c909e049919b27e6d24cfe1c735b
VP <51seer.vincent@gmail.com>
2024-08-07 16:32:04 +0800

    Update ubuntu-tweak.sh

--- Linux/ubuntu-tweak.sh
+++ Linux/ubuntu-tweak.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# Tested on Ubuntu 20.04
+
 # Disable unattended upgrades
 sudo systemctl stop unattended-upgrades
 sudo systemctl disable unattended-upgrades
+
+# Not a fan of snap
+sudo systemctl disable --now snapd
+sudo systemctl disable --now snapd.service
+sudo systemctl disable --now snapd.socket
+sudo systemctl disable --now snapd.seeded
+sudo systemctl disable --now snapd.snap-repair.timer
+systemctl status snapd
+
 # sudo apt-get purge unattended-upgrades
 # I am lazy typing passwds
 sudo sed -i 's/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
