32b5ccd488bf3b925ce8c28d47a93bdd6dd79a68
VP <51seer.vincent@gmail.com>
2025-10-14 18:39:43 +0800

    Added shell scripts for lauching BS on MAC
    Signed-off-by: VP <51seer.vincent@gmail.com>

---
+++ Mac/BS-WS.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# The commands are run directly here as shell commands
+/Applications/BlueStacks.app/Contents/MacOS/BlueStacks --instance Tiramisu64 --cmd launchApp --package "com.gof.global" > /dev/null 2>&1 &
+/Applications/BlueStacks.app/Contents/MacOS/BlueStacks --instance Tiramisu64_1 --cmd launchApp --package "com.gof.global" > /dev/null 2>&1 &
